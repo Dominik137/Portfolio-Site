c0c60461df3cf165e7e7b2fda7a5bf8ae8f9c96f
Dominik Arment <domarment@gmail.com>
2024-02-29 15:55:29 -0500

    yay

--- Portfolio-Site/src/Components/Home.jsx
+++ Portfolio-Site/src/Components/Home.jsx
@@ -8,7 +8,7 @@ function Home(){
         <div className="grid"  >
             <div style={{textAlign: 'center', paddingRight: '10px', paddingLeft: '10px', height: '100vh', paddingBottom: '5px'}}>
                 <h1 style={{paddingTop: '10px', paddingBottom: '23px'}} className="kode-mono">About me</h1>
-                    <article style={{border: '2px solid black'}}>
+                    <article style={{border: '2px solid black', marginTop: '60px'}}>
                         <div style={{fontSize: '20px', fontFamily: 'Roboto-mono'}}>
                     <p>Hello, I'm Dominik. <img width={'100px'}  style={{borderRadius: '50%', border: '2px solid #738678 '}} src="../me.png"/></p>
                     <p>In October 2023, I enrolled in Flatiron School's coding BootCamp in Denver, driven by my passion for software engineering. Completing the program in February 2024, I gained expertise in both front and back-end development.</p>
@@ -21,7 +21,7 @@ function Home(){
             </div>
             <div className="projects-title" style={{textAlign: 'center'}}>
                 <h1 style={{paddingTop: '10px', marginBottom: '0px'}} className="kode-mono">Projects</h1>
-                <p style={{fontSize: '15px', fontWeight: '100'}}>Click to see more info</p>
+                <p style={{fontSize: '15px', fontWeight: '100'}}>Click on a project to see more info</p>
                 <ProjectCard />
             </div>
     
